ae9064470ae3c8a6aa70b53be2089600c320da71
Ashutosh Bhargava <ashutumho@gmail.com>
2021-05-16 14:45:29 +0530

    Collecting the data

---
+++ loadFiles.py
@@ -0,0 +1,46 @@
+import numpy as np
+import pandas as pd
+
+with open("1091.csv", "r") as file:
+    data = file.read()
+
+# spliting the data into rows 
+data = data.split("\n")
+data.pop(-1)
+dataPack = []
+
+# storing the eeg data in list
+for da in data:
+    dataPack.append(da.split(" "))
+
+# making the dataframe
+df = pd.DataFrame(dataPack,columns=["time","signals","value"])
+print(df.head())
+df.to_csv("OriginalData.csv")
+
+# storing the unique signals name
+waveName = df['signals'].unique()
+# separating the Raw and the other wave signals
+rawInd = df['signals'] == waveName[0]
+dfRaw = df[rawInd]
+dfRaw = dfRaw.drop("signals",axis=True)
+dfWaves = df[~rawInd]
+dfRaw.to_csv("allRawData.csv")
+print(dfRaw.head())
+print(dfWaves.head())
+
+df = pd.DataFrame()
+
+for wave in waveName[4:]:
+    df[wave] = dfWaves[dfWaves['signals'] == wave]['value'].values
+
+print(df.head())
+df.to_csv("allWaves.csv")
+
+df = pd.DataFrame()
+
+for wave in waveName[1:4]:
+    df[wave] = dfWaves[dfWaves['signals'] == wave]['value'].values
+
+print(df.head())
+df.to_csv("accelerometerData.csv")
