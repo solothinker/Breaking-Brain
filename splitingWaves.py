0980ed6ee16e7e386e84845430747ab05c089bda
Ashutosh Bhargava <ashutumho@gmail.com>
2021-05-21 00:50:47 +0530

    band separate
    Using following code - https://dsp.stackexchange.com/questions/45345/how-to-correctly-compute-the-eeg-frequency-bands-with-python

---
+++ splitingWaves.py
@@ -0,0 +1,38 @@
+#link-https://dsp.stackexchange.com/questions/45345/how-to-correctly-compute-the-eeg-frequency-bands-with-python
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+fs = 512                                # Sampling rate (512 Hz)
+df = pd.read_csv('allRawData.csv')
+print(df.head(10))
+data = df['value'].values#np.random.uniform(0, 100, 1024)  # 2 
+
+# Get real amplitudes of FFT (only in postive frequencies)
+fft_vals = np.absolute(np.fft.rfft(data))
+
+# Get frequencies for amplitudes in Hz
+fft_freq = np.fft.rfftfreq(len(data), 1.0/fs)
+
+# Define EEG bands
+eeg_bands = {'Delta': (0, 4),
+             'Theta': (4, 8),
+             'Alpha': (8, 12),
+             'Beta': (12, 30),
+             'Gamma': (30, 45)}
+
+# Take the mean of the fft amplitude for each EEG band
+eeg_band_fft = dict()
+for band in eeg_bands:  
+    freq_ix = np.where((fft_freq >= eeg_bands[band][0]) & 
+                       (fft_freq <= eeg_bands[band][1]))[0]
+    eeg_band_fft[band] = np.mean(fft_vals[freq_ix])
+
+# Plot the data (using pandas here cause it's easy)
+df = pd.DataFrame(columns=['band', 'val'])
+df['band'] = eeg_bands.keys()
+df['val'] = [eeg_band_fft[band] for band in eeg_bands]
+ax = df.plot.bar(x='band', y='val', legend=False)
+ax.set_xlabel("EEG band")
+ax.set_ylabel("Mean band Amplitude")
+plt.show()
